55011188ebd99f210b48a2a0acd07a37aa21ec81
zazdergod <buzyrevilya@mail.ru>
2020-05-03 22:35:02 +0300

    some changes with rangemodule and networkservice

---
+++ appPhoRent/appPhoRent/RangeModule/View/RangeTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  RangeTableViewCell.swift
+//  appPhoRent
+//
+//  Created by Ilya Buzyrev on 03.05.2020.
+//  Copyright © 2020 Александр Сетров. All rights reserved.
+//
+
+import UIKit
+
+class RangeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
